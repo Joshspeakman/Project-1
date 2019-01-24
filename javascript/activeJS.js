f351a593bd0ebda249945243d7f42d1eccad8674
Josh S <joshspeakman@Msn.com>
2019-01-24 16:32:10 -0700

    Updated main.css
    Added grids for the cards and media queries for breakpoints.
    
    Added text showding to the main tag line.

--- javascript/activeJS.js
+++ javascript/activeJS.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var queryURL;
     var weatherQueryURL;
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
         var radius = $('#miles').val().trim();
         var queryRadius = '&location.within=' + radius + 'mi';
-       
+
         queryURL = queryURL + queryRadius + queryCity + queryParams;
     }
 
@@ -42,25 +42,25 @@ $(document).ready(function() {
 
     $('#run-search').on('click', function () {
         $("#activities-section").empty();
-        event.preventDefault(); 
+        event.preventDefault();
         buildQueryURL(); //call function to build query
         buildWeatherQueryURL();
 
         $.when(
             $.getJSON(queryURL),
             $.getJSON(weatherQueryURL)
-        ).done(function (response1,response2) {
+        ).done(function (response1, response2) {
 
             console.log(response2);
 
-            response1[0].events.forEach (function(events) {
+            response1[0].events.forEach(function (events) {
 
                 eventDate = events.start.local;
 
                 var dateLength = eventDate.length;
 
-                if (dateLength>10) {
-                activityDate = eventDate.substr(0,10);
+                if (dateLength > 10) {
+                    activityDate = eventDate.substr(0, 10);
                 }
 
                 var eventText = events.name.text;
@@ -73,25 +73,24 @@ $(document).ready(function() {
                 var unixEventDate = moment(activityDate).unix();
                 var regEventDate = moment.unix(unixEventDate).format('YYYY-MM-DD');
 
-                for (i=0; i<response2[0].list.length; i++) {
+                for (i = 0; i < response2[0].list.length; i++) {
                     var tempDateVar = response2[0].list[i].dt;
                     var regWeatherDate = moment.unix(tempDateVar).format('YYYY-MM-DD');
 
                     if (regEventDate === regWeatherDate) {
-                            forecast = response2[0].list[i].weather[0].main;
-                            weatherIconCode = response2[0].list[i].weather[0].icon + '.png';
-                            dayWeatherIconCode = weatherIconCode.replace("n","d");
-                            weatherIcon = 'http://openweathermap.org/img/w/' + dayWeatherIconCode;
-                            
-                            break;
-                    }
-                    else {forecast = 'Weather Not Available Yet For This Date.';
-                        weatherIcon='./images/BLANK_ICON.png';
+                        forecast = response2[0].list[i].weather[0].main;
+                        weatherIconCode = response2[0].list[i].weather[0].icon + '.png';
+                        dayWeatherIconCode = weatherIconCode.replace("n", "d");
+                        weatherIcon = 'http://openweathermap.org/img/w/' + dayWeatherIconCode;
+
+                        break;
+                    } else {
+                        forecast = 'Weather Not Available Yet For This Date.';
+                        weatherIcon = './images/BLANK_ICON.png';
                     }
 
                 }
-                
-        
+
                 var weatherOnDate = forecast;
 
                 $("#activities-section").append(`
@@ -113,26 +112,35 @@ $(document).ready(function() {
                         </div>
                 </div>`);
 
+
+
             });
 
 
         });
+
+        // Scrolls to activities        
+        $('html, body').animate({
+            scrollTop: $('#activities-section').offset().top
+        }, 2500);
+
     });
 
 });
 //  Scroll to top
-$(document).ready(function(){
+$(document).ready(function () {
     $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
-      $(window).scroll(function () {
-          if ($(this).scrollTop() != 0) {
-              $('#toTop').fadeIn();
-          } else {
-              $('#toTop').fadeOut();
-          }
-      }); 
-  $('#toTop').click(function(){
-      $("html, body").animate({ scrollTop: 0 }, 600);
-      return false;
-  });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
-
